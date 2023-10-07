19ac81d4c25366ac471e65ab50e597d9924d0045
Fisurik <96612770+Fisurik@users.noreply.github.com>
2023-10-07 16:54:01 -0700

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,12 +2,56 @@ import { NavLink } from 'react-router-dom';
 import BtnDarkMode from '../btnDarkMode/BtnDarkMode';
 import './style.css';
 
-const Navba = () => {
+const Navbar = () => {
 	const activeLink = 'nav-list__link nav-list__link--active';
 	const normalLink = 'nav-list__link';
 
 	return (
-		
+		<nav className="nav">
+			<div className="container">
+				<div className="nav-row">
+					<NavLink to="/portfolio" className="logo">
+						<strong>Freelancer</strong> portfolio
+					</NavLink>
+
+					<BtnDarkMode />
+
+					<ul className="nav-list">
+						<li className="nav-list__item">
+							<NavLink
+								to="portfolio/"
+								className={({ isActive }) =>
+									isActive ? activeLink : normalLink
+								}
+							>
+								Home
+							</NavLink>
+						</li>
+
+						<li className="nav-list__item">
+							<NavLink
+								to="portfolio/projects"
+								className={({ isActive }) =>
+									isActive ? activeLink : normalLink
+								}
+							>
+								Projects
+							</NavLink>
+						</li>
+						<li className="nav-list__item">
+							<NavLink
+								to="portfolio/contacts"
+								className={({ isActive }) =>
+									isActive ? activeLink : normalLink
+								}
+							>
+								Contacts
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 	);
 };
 
